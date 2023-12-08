47d605daedf5c9c47fe28259e8f7967eecdb597d
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 10:50:15 -0800

    added some functionality to TypingWithSound

--- client/src/components/TypingWithSound.jsx
+++ client/src/components/TypingWithSound.jsx
@@ -1,3 +1,13 @@
 import React, { useEffect } from 'react';
 import Typed from 'typed.js';
 import { Howl } from 'howler';
+
+
+const TypingWithSound = () => {
+    useEffect(() => {
+      const text = 'Welcome to White Chapel Mysteries';
+  
+      // Sound setup using Howler.js
+      const typingSound = new Howl({
+        src: ['path/to/typing-sound.mp3'], // Replace with your audio file path
+      });
