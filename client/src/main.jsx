62032e39719dae23b62aad38fd839d0eb84973b9
Ian Sills <iansills04@gmail.com>
2023-12-08 16:22:59 -0600

    created index.jsx pages modified react-dom/client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Index from './pages/index.jsx'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <h1 className='errorPage'>Wrong page nerd!</h1>,
+    children:[
+      {
+        index: true,
+        element: <Index/>
+      },
+    ]
+  }
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
