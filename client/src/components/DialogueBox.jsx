435a249638cf6b8c079a28ffd71547e0bb788a52
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 11:23:31 -0800

    edited dialogueBox file

--- client/src/components/DialogueBox.jsx
+++ client/src/components/DialogueBox.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import './src/DialogueBox.css';
-import TypingWithSound from './TypingWithSound';
-
+import React, { useState } from 'react';
+import './src/components/DialogueBox.css'; // Import CSS
+import TypingWithSound from './TypingWithSound'; // Import TypingWithSound component
 
 const DialogueBox = () => {
+  const [generatedDialogue, setGeneratedDialogue] = useState('');
+
+  const generateDialogue = () => {
+    // Replace with your dialogue generation logic
+    const newDialogue = 'Your generated dialogue here';
+    setGeneratedDialogue(newDialogue);
+  };
+
   return (
     <div className="dialogue-box">
       <div className="dialogue-text">
-        <p>Sup Alonso !.</p>
+        <p>Hello there! This is an 8-bit dialogue box.</p>
       </div>
-      <TypingWithSound /> {/* Include TypingWithSound component */}
+      <TypingWithSound text={generatedDialogue} /> {}
+      <button onClick={generateDialogue}>Generate Dialogue</button>
     </div>
   );
 };
