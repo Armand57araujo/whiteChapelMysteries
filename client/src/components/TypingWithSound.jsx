672ceaa8514e10df0f5f48925b73f3a953ea700a
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 10:52:35 -0800

    more functionality

--- client/src/components/TypingWithSound.jsx
+++ client/src/components/TypingWithSound.jsx
@@ -5,9 +5,15 @@ import { Howl } from 'howler';
 
 const TypingWithSound = () => {
     useEffect(() => {
-      const text = 'Welcome to White Chapel Mysteries';
-  
-      // Sound setup using Howler.js
-      const typingSound = new Howl({
-        src: ['path/to/typing-sound.mp3'], // Replace with your audio file path
-      });
+        const text = 'Welcome to White Chapel Mysteries';
+
+        // Sound setup using Howler.js
+        const typingSound = new Howl({
+            src: ['path/to/typing-sound.mp3'], // Replace with your audio file path
+        });
+
+        // Create a new instance of Typed.js
+        const typed = new Typed('#typed', {
+            strings: [text],
+            typeSpeed: 50, // Adjust speed as needed
+            onStringTyped: function (arrayPos, self) { 
