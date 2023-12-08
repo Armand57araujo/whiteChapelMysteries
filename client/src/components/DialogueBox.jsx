da555157c48d41ad84e3f982892efb1fef6a78dd
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 11:04:37 -0800

    imported typing sound on Dialogue box

--- client/src/components/DialogueBox.jsx
+++ client/src/components/DialogueBox.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import './src/DialogueBox.css';
+import TypingWithSound from './TypingWithSound';
 
 
 const DialogueBox = () => {
-    return (
-      <div className="dialogue-box">
-        <div className="dialogue-text">
-          <p>Sup Alonso !.</p>
-        </div>
+  return (
+    <div className="dialogue-box">
+      <div className="dialogue-text">
+        <p>Sup Alonso !.</p>
       </div>
-    );
-  };
-  
-  export default DialogueBox;
+      <TypingWithSound /> {/* Include TypingWithSound component */}
+    </div>
+  );
+};
+
+export default DialogueBox;
