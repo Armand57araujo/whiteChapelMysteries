157babde98e5065e4e0e594946ca9aa5ae70a3a7
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 09:53:11 -0800

    added function and export to dialogue box. Also Wassup Alonso

---
+++ client/dialogueBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './dialogueBox.css';
+
+
+const DialogueBox = () => {
+    return (
+      <div className="dialogue-box">
+        <div className="dialogue-text">
+          <p>Sup Alonso !.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default DialogueBox;
