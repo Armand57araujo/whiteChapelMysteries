501a168261e46ece787cf355e0dd90a0daf93d13
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-08 12:10:18 -0800

    added character icon functionality

--- client/src/components/DialogueBox.jsx
+++ client/src/components/DialogueBox.jsx
@@ -3,27 +3,39 @@ import './DialogueBox.css';
 import TypingWithSound from './TypingWithSound'; // Import TypingWithSound component
 
 const DialogueBox = () => {
-  const [generatedDialogue, setGeneratedDialogue] = useState('');
-  const [characterImage, setCharacterImage] = useState(''); // State for character image
+  const [dialogueIndex, setDialogueIndex] = useState(0);
+
+  // Sample dialogue and character images array
+  const dialogues = [
+    { text: 'Dialogue 1', characterImage: 'path_to_image_1.png' },
+    { text: 'Dialogue 2', characterImage: 'path_to_image_2.png' },
+    // Add more dialogues with respective character images as needed
+  ];
 
   const generateDialogue = () => {
-    // Replace with your dialogue generation logic
-    const newDialogue = 'Your generated dialogue here';
-    const newCharacterImage = 'path_to_character_image.png'; // Replace with your image path
-    setGeneratedDialogue(newDialogue);
-    setCharacterImage(newCharacterImage);
+    if (dialogueIndex < dialogues.length) {
+      const currentDialogue = dialogues[dialogueIndex];
+      setDialogueIndex(dialogueIndex + 1);
+
+      // Update dialogue and character image
+      setGeneratedDialogue(currentDialogue.text);
+      setCharacterImage(currentDialogue.characterImage);
+    }
   };
+
+  const [generatedDialogue, setGeneratedDialogue] = useState('');
+  const [characterImage, setCharacterImage] = useState('');
 
   return (
     <div className="dialogue-box">
       <div className="character-icon">
-        {characterImage && <img src={characterImage} alt="Character" />} {/* Display character image */}
+        {characterImage && <img src={characterImage} alt="Character" />}
       </div>
       <div className="dialogue-text">
         <p>Hello there! This is an 8-bit dialogue box.</p>
       </div>
-      <TypingWithSound text={generatedDialogue} /> {/* Include TypingWithSound component */}
-      <button onClick={generateDialogue}>Generate Dialogue</button>
+      <TypingWithSound text={generatedDialogue} />
+      <button onClick={generateDialogue}>Next Dialogue</button>
     </div>
   );
 };
