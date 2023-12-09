642b7660186ba7dfc43c8468b03c7923325e4b53
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-08 16:07:31 -0800

    added files for each page route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Index from './pages/index.jsx'
+import Index from './pages/Index.jsx'
 
 const router = createBrowserRouter([
   {
