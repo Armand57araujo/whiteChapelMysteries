0a317c08a82a9e359cf30c0162b307c50587e0d4
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-09 13:58:26 -0800

    started on signup, but alonso wanted to test it, switching over to homepage

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,12 @@
+import {useState} from 'react';
+
+
+import { ADD_USER } from "../utils/mutations"; 
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+
 const Signup = () => {
+
     return(
         <div>
             <h1>
