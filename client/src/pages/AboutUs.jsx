6c3a4a1e87e6b3ca8ec73f33c54e5f9ca68c11c8
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-09 16:11:12 -0800

    added avatars for each of us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -5,11 +5,36 @@ const AboutUs = () => {
     return(
         <div>
             <Link to="/">
-            <button>Back</button>
+            <button className="backBtn">Back</button>
             </Link>
-            <h1>
+            <h1 className="aboutHeader">
                 About Us
             </h1>
+            <p>
+                White Chapel Mysteries is a game brought to you by the brilliant minds of:
+            </p>
+            <div>
+                <div>
+                <img src="assets/images/avatars/Alonso.jpg"/>
+                    Alonso Ampuero
+                </div>
+                <div>
+                <img src="assets/images/avatars/Armand.jpg"/>
+                    Armand Araujo
+                </div>
+                <div>
+                <img src="assets/images/avatars/Ian.jpg"/>
+                    Ian Sills
+                </div>
+                <div>
+                <img src="assets/images/avatars/Jack.jpg"/>
+                    Jack Lunchick-Seymour
+                </div>
+                <div>
+                <img src="assets/images/avatars/Tayla.jpg"/>
+                    Tayla Rios
+                </div>
+            </div>
         </div>
     )
 }
