a0a3f08a2e708a568c90c7694a3beca2a5b92467
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-10 21:37:31 -0800

    map now renders locations based on the amount of items within the players inventory

--- client/src/components/Inventory.jsx
+++ client/src/components/Inventory.jsx
@@ -1,3 +1,5 @@
+let currentInventory = ['item1','item2','item3','item4','item5']
+
 const Inventory = (props) => {
     return(
         <div>
@@ -5,6 +7,7 @@ const Inventory = (props) => {
             <h1>
                 Inventory
             </h1>
+            {/* takes in visibility prop to "hide" page when clicking office button */}
             <button id="officeBtn" onClick={props.visibility}>Back To Office</button>
         </div>
     )
