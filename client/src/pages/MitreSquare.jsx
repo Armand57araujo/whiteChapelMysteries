2bdd695d135fccc6b531a81435aeb43ff0325b24
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-10 19:09:58 -0800

    got textbox handler to take in location specific dialogue

--- client/src/pages/MitreSquare.jsx
+++ client/src/pages/MitreSquare.jsx
@@ -1,8 +1,25 @@
+
+import { useState } from 'react';
+import TypingWithSound from '/src/components/TypingWithSound.jsx'
+
+const MitreSquareDialogue = ['another victim...','this scene is horrifying','its a god damn arms race']
+
+
 const MitreSquare = () => {
+
+    // const [dialogue, setDialogue] = useState(MitreSquareDialogue.dialogue1.dialogue);
+
+    // const handleDialogueChange = (newDialogue) => {
+    //     setDialogue(newDialogue)
+    // }
+
     return(
         <div>
             <h1>
-                MitreSquare
+                <TypingWithSound 
+                // dialogue = {dialogue} 
+                arr = {MitreSquareDialogue}
+                />
             </h1>
         </div>
     )
