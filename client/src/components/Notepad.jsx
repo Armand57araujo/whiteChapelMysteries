3e9770586b9764432ee973db161c3fb3435c8cfc
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-12-11 21:27:13 -0500

    added localStorage save for notepad

--- client/src/components/Notepad.jsx
+++ client/src/components/Notepad.jsx
@@ -1,11 +1,26 @@
-export default function Notepad() {
+import { useState } from 'react';
+
+
+
+const Notepad = () => {
+
+  const [text, setText] = useState(localStorage.getItem('notes') || '');
+
+  const handleInputChange = (e) => setText(e.target.value);
+  const handleFocusLoss = (e) => localStorage.setItem('notes', text);
+
+
+
   return (
     <>
     <div id='notepad-outer'>
-      <textarea id='notepad-inner'  cols={17} rows={10}>
+      <textarea id='notepad-inner' onBlur={handleFocusLoss} onChange={handleInputChange} value={text} cols={17} rows={10}>
       </textarea>
     </div>
     
     </>
   );
 }
+
+
+export default Notepad;
