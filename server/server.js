893e0b531e210f50c22378cbe962788ad8e77905
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-13 15:32:15 -0800

    testing deployment issues

--- server/server.js
+++ server/server.js
@@ -24,13 +24,13 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  if(process.env.NODE_ENV === 'production') {
+  // if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '..client/dist/index.html'));
     });
-  }
+  // }
   db.once('open', () => { 
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
