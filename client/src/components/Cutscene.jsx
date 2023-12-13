f5e0679091470dc5deec634d13f894e97560e6d5
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-12 23:33:38 -0800

    added code to cutscene component

---
+++ client/src/components/Cutscene.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Cutscene = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    '/images/scene1.png',
+    '/images/scene2.png',
+    // Add more image paths for your cutscene
+  ];
+
+
+
+
+}
